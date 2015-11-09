62b7decc4ec2229a660cf6260ed232d67c14e56e
Kyle Chamberlain (MacbookPro) <kchamberlain@airwavesinc.com>
2015-11-09 14:18:14 -0500

    Got babel working

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-let Trello = require("node-trello"),
+let trello = require("node-trello"),
     config = require('config'),
-    t = new Trello(config.get('trello.key'), config.get('trello.token'));
+    t = new trello(config.get('trello.key'), config.get('trello.token')),
+    _ = require('underscore'),
+    fs = require('fs'),
+    s = require('underscore.string'),
+    util = require('util');
 
-t.get("/1/members/me", function (err, data) {
+t.get("/1/members/me", (err, data) => {
     if (err) throw err;
     console.log(data);
 });
@@ -10,7 +14,7 @@ t.get("/1/members/me", function (err, data) {
 // URL arguments are passed in as an object.
 t.get("/1/members/me", {
     cards: "open"
-}, function (err, data) {
+}, (err, data) => {
     if (err) throw err;
     console.log(data);
 });
